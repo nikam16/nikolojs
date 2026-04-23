d408fcd25eebb6296b3e8e4ad3c70c77d82613ac
nikam1 <makalatia1608@gmail.com>
2026-04-23 09:38:33 +0200

    davaleba js 2

---
+++ davaleba.js
@@ -0,0 +1,75 @@
+/////////////////// 1
+/*
+for (let i=2; i<9;i++) {
+    console.log(i)
+}      */
+/////////////////////////// 2
+/*
+for (let i=5; i<36; i=i+4)
+    console.log(i)
+  */
+ /////////////////////////// 3
+/*
+namravli=1
+
+ for (let i=3; i<9; i++)
+    namravli=namravli*i 
+{
+console.log(namravli)}
+*/
+
+////////////////////////////////4 
+/*
+let person={
+    firstName:"nika",
+    lastName:"makalatia",
+    age:30
+
+}
+console.log(person.firstName,person.lastName)
+*/
+///////////////////////////////////////////  5
+/*
+
+const xelpasi=5001
+if (xelpasi<=1000) {
+    console.log("bonusi= 20% :",(xelpasi*20)/100)
+}
+else if(xelpasi>1000 && xelpasi<=2000) {
+    console.log("bonusi= 10% :",(xelpasi*10)/100)
+}
+else if(xelpasi>2000 && xelpasi<=5000) {
+    console.log("bonusi= 5% :",(xelpasi*5)/100)
+}
+else {
+    console.log("ar gaqvs bonusi")
+}
+
+*/
+/////////////////////////////////////////////  6
+/*
+
+let fruits=["Apple", "Banana", "Orange"]
+{
+console.log(fruits[0],fruits[1],fruits[2])
+}
+*/
+///////////////////////////////////////////// 7
+
+/*
+
+let fruits=["Apple", "Banana", "Orange"]
+fruits.push("Pineapples ")
+fruits.unshift("Grapes ")
+console.log(fruits)
+
+*/
+
+//////////////////////////////////////////// 8
+
+
+let jami=0
+for(i=1;i<34;i++)
+    jami=jami+i
+console.log(jami)
+
